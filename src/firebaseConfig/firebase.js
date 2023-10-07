8a6e386e19fc39a11dc653de8c3655e1c2c5d6d8
Franco Nebro <franconp02@gmail.com>
2023-10-07 01:04:19 -0300

    first commit

---
+++ src/firebaseConfig/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb8i-VNcL7aDUIE0mSnpozewqq7fo2LZ8",
+  authDomain: "shopping-list-2561c.firebaseapp.com",
+  projectId: "shopping-list-2561c",
+  storageBucket: "shopping-list-2561c.appspot.com",
+  messagingSenderId: "1087439443220",
+  appId: "1:1087439443220:web:2ed18c1ebdd75ec2809e01"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
