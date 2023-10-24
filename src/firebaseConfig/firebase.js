e3b4a7ce65ac9d5bf942023ae313746b74834e36
Franco Nebro <franconp02@gmail.com>
2023-10-24 18:56:31 -0300

    analytics

--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import{getFirestore} from '@firebase/firestore'
 
+import { getAnalytics } from "firebase/analytics";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCb8i-VNcL7aDUIE0mSnpozewqq7fo2LZ8",
@@ -9,9 +11,11 @@ const firebaseConfig = {
   projectId: "shopping-list-2561c",
   storageBucket: "shopping-list-2561c.appspot.com",
   messagingSenderId: "1087439443220",
-  appId: "1:1087439443220:web:2ed18c1ebdd75ec2809e01"
+  appId: "1:1087439443220:web:2ed18c1ebdd75ec2809e01",
+  measurementId: "G-B15M5QSL9V"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
+const analytics = getAnalytics(app);
